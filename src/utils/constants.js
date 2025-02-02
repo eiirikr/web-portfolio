e6f0cb86e32d354604a00c2295671ccfb1770081
javierjericm <javierjericm@gmail.com>
2025-02-02 19:14:17 +0800

    Added utils and styles

---
+++ src/utils/constants.js
@@ -0,0 +1,28 @@
+export const SOCIAL_LINKS = [
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/eiirikr/",
+    icon: "fa-brands fa-facebook",
+  },
+  {
+    name: "Telegram",
+    url: "https://t.me/eirikurr",
+    icon: "fa-brands fa-telegram",
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/eiirikr_",
+    icon: "fa-brands fa-square-instagram",
+  },
+  {
+    name: "TikTok",
+    url: "https://tiktok.com/@eirikur_",
+    icon: "fa-brands fa-tiktok",
+  },
+];
+
+export const SKILLS = {
+  frontend: ["HTML", "CSS", "JavaScript", "Bootstrap", "React"],
+  backend: ["PHP", "Java", "Python", "C++"],
+  database: ["MySQL"],
+};
