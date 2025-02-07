1d47550932456ebc70ba07e49abd30c702d8a3e6
javierjericm <javierjericm@gmail.com>
2025-02-07 23:42:01 +0800

    Added scrollReveal

--- src/utils/scrollReveal.js
+++ src/utils/scrollReveal.js
@@ -1,22 +1,22 @@
-// import ScrollReveal from "scrollreveal";
+import ScrollReveal from "scrollreveal";
 
-// export const sr = ScrollReveal({
-//   origin: "top",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true,
-// });
+export const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
 
-// export const srLeft = ScrollReveal({
-//   origin: "left",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true,
-// });
+export const srLeft = ScrollReveal({
+  origin: "left",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
 
-// export const srRight = ScrollReveal({
-//   origin: "right",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true,
-// });
+export const srRight = ScrollReveal({
+  origin: "right",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
