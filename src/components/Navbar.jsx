86ad2acbe5ee50fab935126af3aa5308074ebb6c
javierjericm <javierjericm@gmail.com>
2025-02-08 23:41:00 +0800

    Improved NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,58 +14,47 @@ const Navbar = () => {
         <p className="nav-name">Jer</p>
         <span>.</span>
       </div>
+
       <div
         className={`nav-menu ${isMenuOpen ? "responsive" : ""}`}
         id="myNavMenu"
       >
         <ul className="nav_menu_list">
-          <li className="nav_list">
-            <Link
-              to="home"
-              smooth={true}
-              duration={500}
-              className="nav-link active-link"
-            >
-              Home
-            </Link>
-            <div className="circle"></div>
-          </li>
-          <li className="nav_list">
-            <Link to="about" smooth={true} duration={500} className="nav-link">
-              About
-            </Link>
-            <div className="circle"></div>
-          </li>
-          <li className="nav_list">
-            <Link
-              to="projects"
-              smooth={true}
-              duration={500}
-              className="nav-link"
-            >
-              Projects
-            </Link>
-            <div className="circle"></div>
-          </li>
-          <li className="nav_list">
-            <Link
-              to="contact"
-              smooth={true}
-              duration={500}
-              className="nav-link"
-            >
-              Contact
-            </Link>
-            <div className="circle"></div>
-          </li>
+          {["home", "about", "projects", "contact"].map((section, index) => (
+            <li className="nav_list" key={index}>
+              <Link
+                to={section}
+                smooth={true}
+                duration={500}
+                spy={true} // Makes the active link dynamic
+                offset={-70} // Adjusts for fixed navbar height if needed
+                activeClass="active-link"
+                className="nav-link"
+              >
+                {section.charAt(0).toUpperCase() + section.slice(1)}
+              </Link>
+              <div className="circle"></div>
+            </li>
+          ))}
         </ul>
       </div>
-      <div class="nav-button">
-        <a href="Jeric Javier.pdf" download="Jeric Javier.pdf" class="btn">
-          Download CV <i class="uil uil-file-alt"></i>
+
+      <div className="nav-button">
+        <a
+          href="Jeric Javier.pdf"
+          download="Jeric Javier.pdf"
+          className="btn"
+          aria-label="Download CV"
+        >
+          Download CV <i className="uil uil-file-alt"></i>
         </a>
       </div>
-      <div className="nav-menu-btn" onClick={toggleMenu}>
+
+      <div
+        className="nav-menu-btn"
+        onClick={toggleMenu}
+        aria-label="Toggle Menu"
+      >
         <i className="uil uil-bars"></i>
       </div>
     </nav>
